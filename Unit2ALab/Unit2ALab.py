383caa151593fca0e495c8d63e1c3f2bd9f694a5
AllenHuo <19huomingchao@bprep.org>
2018-09-11 09:20:39 -0700

    Lab

---
+++ Unit2ALab/Unit2ALab.py
@@ -0,0 +1,27 @@
+x = 4
+y = 1.5
+print(int(x + y))
+
+str = input("Select a Number")
+print(int(str))
+
+i = 100
+j = 200
+x = 300
+y = 100
+if i == j or i > x:
+    print("true")
+else:
+    print("false")
+if i < j and i>= y:
+    print("true")
+else:
+    print("false")
+if i != j and i != x:
+    print("true")
+else:
+    print("false")
+if i < j and i < x and i < y:
+    print("true")
+else:
+    print("false")
