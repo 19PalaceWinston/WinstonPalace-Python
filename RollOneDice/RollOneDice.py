4e0a854362d0ccec1eb8c01ee1edeebebbe343de
AllenHuo <19palacewinston@bprep.org>
2018-10-31 10:04:09 -0700

    Dice Game

--- RollOneDice/RollOneDice.py
+++ RollOneDice/RollOneDice.py
@@ -91,34 +91,52 @@ def challenge1(dice):
     print("Rolls:",rolls)
 
 def challenge2(dice):
-    one = 0
     two = 0
     three = 0
     four = 0
     five = 0
     six = 0
+    seven = 0
+    eight = 0
+    nine = 0
+    ten = 0
+    eleven = 0
+    twelve = 0
     many = 2000
     dieNum = []
     for q in range(0,many):
         dieNum.append(random.randint(0,5))
+    for b in range(0,len(dieNum)-1,2):
+        current = dieNum[b] + dieNum[b+1] + 2
+        if current == 2:
+            two = two + 1
+        if current == 3:
+            three = three + 1
+        if current == 4:
+            four = four + 1
+        if current == 5:
+            five = five + 1
+        if current == 6:
+            six = six + 1
+        if current == 7:
+            seven = seven + 1
+        if current == 8:
+            eight = eight + 1
+        if current == 9:
+            nine = nine + 1
+        if current == 10:
+            ten = ten + 1
+        if current == 11:
+            eleven = eleven + 1
+        if current == 12:
+            twelve = twelve + 1
+        current = 0
+
     for z in range(0,len(dice[0])):
         for y in range(0,many):
             print(dice[dieNum[y]][z],end=" ")
         print()
-    for r in range(0,many):
-        if dieNum[r] == 0:
-            one = one + 1
-        if dieNum[r] == 1:
-            two = two + 1
-        if dieNum[r] == 2:
-            three = three + 1
-        if dieNum[r] == 3:
-            four = four + 1
-        if dieNum[r] == 4:
-            five = five + 1
-        if dieNum[r] == 5:
-            six = six + 1
     print()
-    print("One:",one,"\nTwo:",two,"\nThree:",three,"\nFour",four,"\nFive",five,"\nSix:",six)
+    print("\nTwo:",two,"\nThree:",three,"\nFour",four,"\nFive",five,"\nSix:",six,"\nSeven:",seven,"\nEight:",eight,"\nNine:",nine,"\nTen:",ten,"\nEleven:",eleven,"\nTwelve:",twelve)
 
 main()
